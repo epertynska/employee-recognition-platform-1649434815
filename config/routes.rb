0191f87ffb281921d1fea7ce61982249b313e561
epertynska <epertynska@gmail.com>
2022-04-11 14:47:16 +0200

    Revert "Create home_spec.rb"
    This reverts commit 8cbef4e0a9936eca35a03a2662541bb94b96e838.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :employees
   # resources :ideas
   get 'home/index'
+  get '/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
