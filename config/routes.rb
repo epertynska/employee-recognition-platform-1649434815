23743c735d9cc73315461c067f8d17a4319390ba
epertynska <epertynska@gmail.com>
2022-04-12 18:57:23 +0200

    permissions for kudos' authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :kudos
   devise_for :employees
-  # resources :ideas
   get 'home/index'
-  root 'home#index'
+  root 'kudos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+(13 sloc)  709 Bytes
+   
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,6 +8,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-    1.upto(5) do |i|
-        Employee.create(email: "employee#{i}@sample.com", password: "123456")
+    6.upto(9) do |i|
+        Employee.create!(email: "employee#{i}@sample.com", password: "123456")
+    end
+
+    6.upto(9) do |j|
+        Kudo.create!(title: "#{j}th kudo", content: "seeds generated one by one", giver: Employee.find_by(email: "employee2@sample.com"), receiver: Employee.find_by(email: "employee4@sample.com"))
     end
